61496f5fd7eca7121bec0a977007f0f9250dfdee
indy <indynet00@gmail.com>
2025-10-17 15:27:47 -0500

    remove nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,14 +1,5 @@
 {
   description = "nixos configuration flake";
 
-  inputs      = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
-
-  outputs     = { self, nixpkgs, ... }:
-    let system = "x86_64-linux";
-        pkgs   = import nixpkgs { inherit system; };
-    in {
-      home = import ./home.nix;
-    };
+  outputs     = { self, nixpkgs, ... }: { home = import ./home.nix; };
 }
