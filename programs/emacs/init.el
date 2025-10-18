07009aed697438d9e1d7106781f55b35572e861d
indy <indynet00@gmail.com>
2025-10-17 20:27:52 -0500

    mk:basic mc kbds

--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -79,6 +79,14 @@
 
 (require 'direnv)
 
-
 (setq direnv-always-show-summary nil)
 (direnv-mode)
+
+;; multiple-cursors
+
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-<") 'mc/mark-previous-line-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C->") 'mc/mark-next-like-thisx)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
